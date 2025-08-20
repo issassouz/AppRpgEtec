59f8fa477fb04efa6d961657573320fe44efeea2
etechas <etechas@LI02-17>
2025-08-19 22:30:39 -0300

    Add project files.

---
+++ Services/Usuarios/UsuarioService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppRpgEtec.Models;
+
+namespace AppRpgEtec.Services.Usuarios
+{
+    internal class UsuarioService : Request 
+    {
+        private readonly Request _request;
+        private const string _apiUrlBase = "isabelinha-rpg.azurewebsites.net";
+
+        public UsuarioService()
+        {
+            _request = new Request();
+        }
+
+        public async Task<Usuario> PostResgistraUsuarioAsync(Usuario u)
+        {
+            string urlComplementar = "/Resgistrar";
+            u.Id = await _request.PostReturnIntAsync(_apiUrlBase + urlComplementar, u, string.Empty);
+
+            return u;
+        }
+
+        public async Task<Usuario> PostAutenticarUsuarioAsync(Usuario u)
+        {
+            string urlComplementar = "/Autenticar";
+            u = await _request.PostAsync(_apiUrlBase + urlComplementar, u, string.Empty);
+
+            return u;
+        }
+
+
+
+
+    }
+}
---
+++ ViewModels/Usuarios/UsuarioViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppRpgEtec.Models;
+using AppRpgEtec.Services.Usuarios;
+using System.Windows.Input;
+
+namespace AppRpgEtec.ViewModels.Usuarios
+{
+    public class UsuarioViewModel : BaseViewModel
+    {
+        public UsuarioViewModel()
+        {
+            uService = new UsuarioService();
+            InicializarCommands();
+        }
+        public void InicializarCommands()
+        {
+            AutenticarCommand = new Command(async () => await AutenticarUsuario());
+        }
+
+        private UsuarioService uService;
+        public ICommand AutenticarCommand { get; set; }
+ 
+        #region AtributoPropriedades
+        private string login = string.Empty;
+        private string senha = string.Empty;
+
+        public string Login 
+        {   get => login;
+            set
+            {
+                login = value;
+                OnPropertyChanged();
+            }
+
+        }
+        public string Senha
+        {   get => senha;
+            set 
+            {
+                senha = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Metodos 
+        public async Task AutenticarUsuario()
+        {
+            try
+            {
+                Usuario u = new Usuario();
+                u.Username = login;
+                u.PasswordString = senha;
+
+                Usuario uAutenticado = await uService.PostAutenticarUsuarioAsync(u);
+
+                if (!string.IsNullOrEmpty(uAutenticado.Token))
+                {
+                    string message = $"Bem vindo(a) {uAutenticado.Username}";
+
+                    //Guardando dados para uso futuro
+                    Preferences.Set("UsuarioId", uAutenticado.Id);
+                    Preferences.Set("UsuarioUsername", uAutenticado.Username);
+                    Preferences.Set("UsuarioPerfil", uAutenticado.Perfil);
+                    Preferences.Set("UsuarioToken", uAutenticado.Token);
+
+                    await Application.Current.MainPage.DisplayAlert("Informação", message, "Ok");
+
+                    Application.Current.MainPage = new MainPage();
+                }
+                else
+                {
+                    await Application.Current.MainPage
+                        .DisplayAlert("Irformação", "Dados incorretos", "Ok");
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Informação", ex.Message + ex.InnerException, "Ok");
+            }
+
+        }
+
+        #endregion
+    }
+}
